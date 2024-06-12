8ad08dbdd94f990dc9a5d998bd946645089fb99d
gagandeepsingh21 <gagandeepgahir@gmail.com>
2024-06-12 17:03:38 +0300

    added movies to rows

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Main from '../Main'
+import Row from '../Row'
+import requests from '../../Request'
 
 const Home = () => {
   return (
     <div>
         <Main />
+        <Row title='UpComing' fetchUrl={requests.requestUpcoming} />
+        <Row title='Popular' fetchUrl={requests.requestPopular} />
+        <Row title='Trending' fetchUrl={requests.requestTrending} />
+        <Row title='Top Rated' fetchUrl={requests.requestTopRated} />
+        <Row title='Horror' fetchUrl={requests.requestHorror} />
+
     </div>
   )
 }

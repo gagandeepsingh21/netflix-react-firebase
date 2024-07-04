54ab31600978e08b429eb2d56c02286796c59557
gagandeepsingh21 <gagandeepgahir@gmail.com>
2024-07-04 16:58:37 +0300

    finished working on the account section and deployed on firebase

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,8 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
-
+import { UserAuth } from '../context/AuthContext';
+import { db } from '../firebase';
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore';
 const Movie = ({x}) => {
 const [Like, setLike] =useState(false);
+const [saved, setSaved] = useState(false);
+const {user} = UserAuth();
+
+const movieId = doc(db, 'users', `${user?.email}`)
+const savedMovies = async () =>{
+  if(user?.email){
+    setLike(!Like)
+    setSaved(true)
+    await updateDoc(movieId,{
+      savedMovies:arrayUnion({
+        id:x.id,
+        title: x.title,
+        img:x.backdrop_path
+      })
+    })
+  }else{
+    alert('Please log in to save a movie')
+  }
+}
 
   return (
     <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
@@ -15,7 +36,7 @@ const [Like, setLike] =useState(false);
         <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
           {x?.title}
         </p>
-        <p>
+        <p onClick={savedMovies}>
           {Like ? (
             <FaHeart className="absolute top-4 left-4 text-gray-300" />
           ) : (
